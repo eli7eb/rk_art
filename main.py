6fdaf82bfe87ed7644f0a7f358e4428d02ebd35a
eli7eb <eli7eb@gmail.com>
2021-11-29 17:33:33 +0200

    get mood value

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy import Config
+from kivy.uix.widget import Widget
+
 Config.set('graphics', 'multisamples', '0')
+
 
 class RkMainApp(App):
     def build(self):
         self.root = Builder.load_file('rk.kv')
 
-    def process(self):
-        mood_text = self.root.ids.mood
-        print(mood_text)
-
 
 class MainWindow(Screen):
+
+    def process(self):
+        print(self.ids.mood.text)
+
+    def go_play(self):
+        mood_text = self.ids.mood.text
     pass
+
 
 class OptionsWindow(Screen):
     pass
